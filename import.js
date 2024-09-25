83e0ca82f377caa083bf5858c8126a5d0c34df75
Samy <s_rahim@estin.dz>
2024-09-25 17:53:25 +0100

    npm i

--- import.js
+++ import.js
@@ -1,13 +1,23 @@
+import { readdirSync, readFileSync } from "fs";
 import weaviate from "weaviate-ts-client";
-import { readDirSync } from "fs";
+
+// Create a Weaviate client instance
 const client = weaviate.client({
   scheme: "http",
   host: "localhost:8080",
 });
-let i = 0;
-for (const img of readDirSync("./img")) {
-  const b64 = Buffer.from(img).toString("base64");
 
+let i = 0;
+const imageDir = "./img";
+
+// Loop through all the files in the directory
+for (const imgFileName of readdirSync(imageDir)) {
+  const imgPath = `${imageDir}/${imgFileName}`;
+  // Read the image file and convert it to base64
+  const imgBuffer = readFileSync(imgPath);
+  const b64 = imgBuffer.toString("base64");
+
+  // Create an object in Weaviate with the image data
   await client.data
     .creator()
     .withClassName("Images")
