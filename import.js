cbf035799823c49620f94ce40c53565b275a9bc3
Samy <s_rahim@estin.dz>
2024-09-25 17:51:05 +0100

    npm i

---
+++ import.js
@@ -0,0 +1,20 @@
+import weaviate from "weaviate-ts-client";
+
+const client = weaviate.client({
+  scheme: "http",
+  host: "localhost:8080",
+});
+let i = 0;
+for (const img of readDirSync("./img")) {
+  const b64 = Buffer.from(img).toString("base64");
+
+  await client.data
+    .creator()
+    .withClassName("Images")
+    .withProperties({
+      image: b64,
+      text: "Image " + i,
+    })
+    .do();
+  i++;
+}
