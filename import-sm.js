675ce4bfa410d9e807d839adc3631b3dc045264f
Samy <s_rahim@estin.dz>
2024-10-01 20:49:43 +0100

    h

--- import-sm.js
+++ import-sm.js
@@ -1,75 +1,75 @@
-import weaviate from "weaviate-ts-client";
+import weaviate from "weaviate-client";
 import sharp from "sharp";
 
-const client = weaviate.client({
-  scheme: "http",
-  host: "localhost:8080",
-});
-const list = [];
-for (let page = 1; page < 2; page++) {
-  const resp = await fetch(
-    `https://www.stepmode.dz/wp-json/wc/store/products?per_page=50&page=${page}`
-  );
-  if (!resp.ok) {
-    console.log("error");
-    break;
-  }
-  const data = await resp.json();
+const client = await weaviate.connectToLocal();
 
-  list.push(...data);
-}
+console.log(client);
 
-console.log(list.length);
+// const list = [];
+// for (let page = 1; page < 2; page++) {
+//   const resp = await fetch(
+//     `https://www.stepmode.dz/wp-json/wc/store/products?per_page=50&page=${page}`
+//   );
+//   if (!resp.ok) {
+//     console.log("error");
+//     break;
+//   }
+//   const data = await resp.json();
 
-// remove duplicates
+//   list.push(...data);
+// }
 
-const seen = new Set();
-const newList = list.filter((el) => {
-  const duplicate = seen.has(el.id);
-  seen.add(el.id);
-  return !duplicate;
-});
+// console.log(list.length);
 
-const promises = newList.map(async (product, i) => {
-  if (i > 21) {
-    return;
-  }
-  if (!product.images[0]?.src || !product.slug) {
-    return;
-  }
+// // remove duplicates
 
-  const img = await fetch(product.images[0]?.src);
-  if (!img.ok) {
-    return product;
-  }
-  const contentType = img.headers.get("content-type");
-  console.log("content type : ", contentType);
+// const seen = new Set();
+// const newList = list.filter((el) => {
+//   const duplicate = seen.has(el.id);
+//   seen.add(el.id);
+//   return !duplicate;
+// });
 
-  const imgBuffer = await img.arrayBuffer();
-  let imgBase64 = Buffer.from(imgBuffer).toString("base64");
-  if (!contentType.includes("avif")) {
-    const jpgBuffer = await sharp(imgBuffer).jpeg().toBuffer();
+// const promises = newList.map(async (product, i) => {
+//   if (i > 21) {
+//     return;
+//   }
+//   if (!product.images[0]?.src || !product.slug) {
+//     return;
+//   }
 
-    // Convert the .jpg buffer to base64
-    imgBase64 = jpgBuffer.toString("base64");
-  }
-  console.log("done");
-  console.log("product : ", product.slug);
-  console.log("image : ", imgBase64);
+//   const img = await fetch(product.images[0]?.src);
+//   if (!img.ok) {
+//     return product;
+//   }
+//   const contentType = img.headers.get("content-type");
+//   console.log("content type : ", contentType);
 
-  const resppp = await client.data
-    .creator()
-    .withClassName("Images")
-    .withProperties({
-      image: imgBase64,
-      text: product.slug,
-    })
-    .do();
+//   const imgBuffer = await img.arrayBuffer();
+//   let imgBase64 = Buffer.from(imgBuffer).toString("base64");
+//   if (!contentType.includes("avif")) {
+//     const jpgBuffer = await sharp(imgBuffer).jpeg().toBuffer();
 
-  console.log("done1 : ", resppp);
-  console.log("done2\n");
-});
+//     // Convert the .jpg buffer to base64
+//     imgBase64 = jpgBuffer.toString("base64");
+//   }
+//   console.log("done");
+//   console.log("product : ", product.slug);
+//   console.log("image : ", imgBase64);
 
-await Promise.all(promises);
+//   const resppp = await client.data
+//     .creator()
+//     .withClassName("Images")
+//     .withProperties({
+//       image: imgBase64,
+//       text: product.slug,
+//     })
+//     .do();
 
-console.log("length list : ", newList.length);
+//   console.log("done1 : ", resppp);
+//   console.log("done2\n");
+// });
+
+// await Promise.all(promises);
+
+// console.log("length list : ", newList.length);
