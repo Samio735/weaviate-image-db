c04d547fce1df4c45178b150f7f5badb3222b45b
Samy <s_rahim@estin.dz>
2024-10-01 22:14:50 +0100

    h

--- query.js
+++ query.js
@@ -22,7 +22,7 @@ async function convertAndSearchImage() {
     // Perform image search in Weaviate
     const result = await images.query.nearImage(jpgBuffer, {
       returnProperties: ["text"],
-      limit: 4,
+      limit: 20,
     });
 
     console.log(JSON.stringify(result.objects, null, 2));
